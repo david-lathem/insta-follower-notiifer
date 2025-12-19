6b5123c6f5e59b57793d11cd9d5d6e19b6aa4189
bilal-the-dev <bilalfarooq6001@gmail.com>
2025-12-19 22:00:16 +0500

    liux

--- src/login.js
+++ src/login.js
@@ -1,13 +1,17 @@
-import puppeteer from "puppeteer";
+const puppeteer = require("puppeteer");
 
-const browser = await puppeteer.launch({
-  headless: false,
-  userDataDir: "./data",
-  executablePath: "/usr/bin/chromium-browser",
-});
+async function login() {
+  const browser = await puppeteer.launch({
+    headless: false,
+    userDataDir: "./data",
+    executablePath: "/usr/bin/chromium-browser",
+  });
 
-const page = await browser.newPage();
+  const page = await browser.newPage();
 
-await page.goto("https://instagram.com/");
+  await page.goto("https://instagram.com/");
 
-await page.setViewport({ width: 1526, height: 698 });
+  await page.setViewport({ width: 1526, height: 698 });
+}
+
+login();
