ae6d0258e1c0dd6fa55703bc054f3926753e3be0
bilal-the-dev <bilalfarooq6001@gmail.com>
2025-12-21 16:14:19 +0500

    add sqlite

--- src/login.js
+++ src/login.js
@@ -5,10 +5,18 @@ async function login() {
     headless: false,
     userDataDir: "./data",
     executablePath: "/usr/bin/chromium-browser",
-    args: ["--no-sandbox"],
+    args: [
+      "--no-sandbox",
+      `--proxy-server=https://${process.env.PROXY_HOST}:${process.env.PROXY_PORT}`,
+    ],
   });
 
   const page = await browser.newPage();
+
+  await page.authenticate({
+    username: process.env.PROXY_USERNAME,
+    password: process.env.PROXY_PASSWORD,
+  });
 
   await page.goto("https://instagram.com/");
 
