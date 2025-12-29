fa314f863cbad7d836b50cb543f6015aa3faf799
bilal-the-dev <bilalfarooq6001@gmail.com>
2025-12-29 17:39:04 +0500

    a

--- src/utils/movements.js
+++ src/utils/movements.js
@@ -5,6 +5,11 @@ const fs = require("fs");
 exports.createPageWithGhostCursor = async (browser, url) => {
   const page = await browser.newPage();
 
+  await page.setViewport({
+    width: 1366,
+    height: 768,
+  });
+
   await page.setUserAgent(
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
   );
